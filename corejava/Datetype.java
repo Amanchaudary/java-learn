9541971373a68d1d03dd5da46db99e8e7fddf262
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-02-03 22:14:36 +0900

    datatype

---
+++ corejava/Datetype.java
@@ -0,0 +1,12 @@
+public class Datetype {
+        public static void main(String[] args) {
+            boolean aman=true;
+            char c ='c';
+            double d = 19.99;
+            float f=3.4f;
+            System.out.println(f);
+            System.out.println(c);
+            System.out.println(aman);
+            System.out.println(d);
+        }
+}
