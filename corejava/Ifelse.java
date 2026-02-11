803a5351bb790b39eb034117f342374177170de1
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-02-11 17:48:55 +0900

    if else and swtch in java learn

---
+++ corejava/Ifelse.java
@@ -0,0 +1,15 @@
+public class Ifelse {
+   public static void main(String[] args) {
+    
+     int no=5;
+     if(no>5){
+        System.out.println("aman");
+     }
+     else if(no==5){
+        System.out.println("this is good");
+     }
+        else{
+        System.out.println("chaudhary");
+     }
+   }
+}
