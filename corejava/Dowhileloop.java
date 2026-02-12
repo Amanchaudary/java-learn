76be3c450cfc489d30f1275f6ca541b8ae967c0d
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-02-12 21:03:28 +0900

    learn about forloop,while and do-while loop

---
+++ corejava/Dowhileloop.java
@@ -0,0 +1,9 @@
+public class Dowhileloop {
+    public static void main(String[] args) {
+        int i=1;
+        do {
+            System.out.println("hello aman");
+            i++;
+        } while (i>=10);
+    }
+}
---
+++ corejava/Forloop.java
@@ -0,0 +1,22 @@
+public class Forloop {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+
+        // for(int i=0;i<=6;i++){
+        // System.out.println(i);
+        // System.out.println(i*2);
+        // }
+        int i = 0;
+        while (i <= 17) {
+            System.out.println(i);
+            i = i + 2;
+
+        }
+
+    }
+
+}
