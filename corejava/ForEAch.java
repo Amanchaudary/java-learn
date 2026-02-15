b18e9d578d6ab9f4ad277a708ff3c65189758de4
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-02-15 22:27:27 +0900

    learan about method and what is mean by public  static void main

---
+++ corejava/ForEAch.java
@@ -0,0 +1,9 @@
+public class ForEAch {
+        public static void main(String[] args) {
+            int []number={10,20,30,49,50};
+            // for-each loop
+            for(int num:number){
+                System.out.println(num);
+            }
+        }
+}
---
+++ corejava/Method.java
@@ -0,0 +1,23 @@
+public class Method {
+    public static void main(String[] args) {
+        System.out.println("today learn about method");
+        greet();
+        average(4,6);
+        hy();
+        
+    }
+    public static void hy(){
+        System.out.println("this is method video for learing");
+    }
+    
+    public static void greet(){
+        System.out.println("helloworld");
+    }
+
+    public static void average(int a,int b){
+        int avg=(a+b)/2;
+        System.out.println("the average is="+avg);
+    }
+
+
+}
