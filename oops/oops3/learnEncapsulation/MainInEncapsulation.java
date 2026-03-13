bfd272aac6468570f7d9daf9f5416da09896fda6
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-03-13 21:53:16 +0900

    pillers 3 encapsulation learn about and make getters and setters

---
+++ oops/oops3/learnEncapsulation/MainInEncapsulation.java
@@ -0,0 +1,12 @@
+package oops3.learnEncapsulation;
+
+import oops3.LearnPackage.Person;
+
+public class MainInEncapsulation {
+    public static void main(String[] args) {
+        Person p1=new Person();
+        p1.setAge(12);
+        System.out.println(p1.getAge());
+    }
+    
+}
