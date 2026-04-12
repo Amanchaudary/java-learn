3ae1bbd8fcc3666bb7eea6816ba607b80f4935dc
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-04-12 23:15:24 +0900

    learn String method and stringbuilder and stringbuffer methods

---
+++ Strings2/Demo.java
@@ -0,0 +1,13 @@
+public class Demo{
+    public static void main(String[] args) {
+        String s1=new String("");
+        String s2=new String("Hello");
+        System.out.println(s1);
+        System.out.println(s2);
+        String s3="aman";
+        String s4=new String(s3);
+        char[] arr={ 'a','m','a','n'};
+        System.out.println(arr);
+        System.out.println(s4);
+    }
+}
