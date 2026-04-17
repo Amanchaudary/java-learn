eab5bf550c8a4861c6781699347a67641183f6b5
チャウダリアマン <146607332+Amanchaudary@users.noreply.github.com>
2026-04-17 21:09:19 +0900

    learn about Generic and opcasting and downcasting

--- java_nested_class/Demo3.java
+++ java_nested_class/Demo3.java
@@ -1,7 +1,7 @@
 public class Demo3 {
     public static void main(String[] args) {
-        Outer outer=new Outer();
-        outer.greet();
+        // Outer outer=new Outer();
+        // outer.greet();
     }
 }
 class Outer{
